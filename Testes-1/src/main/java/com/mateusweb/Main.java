9517fe71517e3659f32da1a466e068d4fcedfc1c
Mateus <128904463+MattewGG@users.noreply.github.com>
2025-03-26 15:23:11 -0300

    feature: created initial struture for the project, classes Filezip and dowload are adicioned and depencencie jsoup

---
+++ Testes-1/src/main/java/com/mateusweb/Main.java
@@ -0,0 +1,7 @@
+package com.mateusweb;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Testes-1/src/main/java/com/mateusweb/services/FileDowload.java
@@ -0,0 +1,5 @@
+package com.mateusweb.services;
+
+public class FileDowload {
+
+}
