f0ac367862641b2f8b6795983f35f85236056c3d
Mateus <128904463+MattewGG@users.noreply.github.com>
2025-03-27 00:32:25 -0300

    feature: created a method for zip files

--- Testes-1/src/main/java/com/mateusweb/Main.java
+++ Testes-1/src/main/java/com/mateusweb/Main.java
@@ -1,6 +1,7 @@
 package com.mateusweb;
 
 import com.mateusweb.services.FileDowload;
+import com.mateusweb.services.FileZip;
 
 import java.io.IOException;
 
@@ -9,6 +10,7 @@ public class Main {
 
 
         FileDowload.downloadFiles("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos", "downloads");
+        FileZip.zipfile("downloads//my_files.zip", new String[]{"downloads/Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf", "downloads/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"});
 
     }
 }
--- Testes-1/src/main/java/com/mateusweb/services/FileZip.java
+++ Testes-1/src/main/java/com/mateusweb/services/FileZip.java
@@ -1,4 +1,39 @@
 package com.mateusweb.services;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class FileZip {
+
+    public static void zipfile(String outputfile, String[] inputfiles) throws IOException {
+        try (
+                FileOutputStream destino = new FileOutputStream(new File(outputfile));
+                ZipOutputStream out = new ZipOutputStream(destino)
+        ) {
+            for (String inputfile : inputfiles) {
+                File file = new File(inputfile);
+                if (!file.exists()) {
+                    System.err.println("file not found: " + inputfile);
+                    continue;
+                }
+
+                try (FileInputStream streamDeEntrada = new FileInputStream(file)) {
+                    ZipEntry entry = new ZipEntry(file.getName());
+                    out.putNextEntry(entry);
+
+                    byte[] buffer = new byte[8192];
+                    int bytesRead;
+                    while ((bytesRead = streamDeEntrada.read(buffer)) != -1) {
+                        out.write(buffer, 0, bytesRead);
+                    }
+
+                    out.closeEntry();
+                }
+            }
+        }
+    }
 }
